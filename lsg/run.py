b1655278b30c0e8fc4089ac927124f820c8f1fcb
Nemanja Milićević <the.nemanja.milicevic@gmail.com>
2019-03-10 00:01:31 +0100

    Visualize optimal learning space

--- lsg/run.py
+++ lsg/run.py
@@ -2,12 +2,30 @@ import random
 
 import neat
 import pandas as pd
+import pydot
 
 from .evaluation import LearningSpaceEvaluator
 from .genome import LearningSpaceGenome
 
 
 NUM_QUESTIONS = 4
+
+
+def save_learning_space_graph(knowledge_states, output='graph.png'):
+    edges = []
+
+    for source_idx, source_state in enumerate(knowledge_states[:-1]):
+        for dst_state in knowledge_states[source_idx + 1:]:
+            if sum((source_state ^ dst_state)._bitarray) == 1:
+                src = str(source_state)
+                dst = str(dst_state)
+                edges.append((src, dst))
+
+    graph = pydot.graph_from_edges(edges, directed=True)
+    graph_image_bytes = graph.create_png(prog='dot')
+
+    with open('./graph.png', 'wb') as fp:
+        fp.write(graph_image_bytes)
 
 
 def run(config_file, response_patterns):
@@ -20,8 +38,8 @@ def run(config_file, response_patterns):
     population = neat.Population(config)
     population.add_reporter(neat.StdOutReporter(show_species_detail=True))
     evaluator = LearningSpaceEvaluator(response_patterns)
-    winner = population.run(evaluator.evaluate_genomes, 20)
-    print(winner.knowledge_states())
+    optimal_ls = population.run(evaluator.evaluate_genomes, 10)
+    return optimal_ls
 
 
 def load_response_patterns():
@@ -38,4 +56,7 @@ def load_response_patterns():
 if __name__ == '__main__':
     config_file = 'config/default.ini'
     response_patterns = load_response_patterns()
-    run(config_file, response_patterns)
+    optimal_ls = run(config_file, response_patterns)
+    knowledge_states = sorted(optimal_ls.knowledge_states(),
+                              key=lambda state: sum(state._bitarray))
+    save_learning_space_graph(knowledge_states)
