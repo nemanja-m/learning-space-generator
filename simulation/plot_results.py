890ba703b4628d37ae07e91557667a04002ddf61
Nemanja Milićević <the.nemanja.milicevic@gmail.com>
2019-08-14 22:29:14 +0200

    Plot mean and std metric values

--- simulation/plot_results.py
+++ simulation/plot_results.py
@@ -1,27 +1,49 @@
 import argparse
+import glob
 import json
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def extract_metric(results, metric_name):
-    neat_results = results.get('NEAT')
-    iita_results = results.get('IITA')
-    return zip(*[(neat_metrics.get(metric_name), iita_metrics.get(metric_name))
-                 for neat_metrics, iita_metrics in zip(neat_results, iita_results)])
+def extract_metric(simulation_results, metric_name):
+    neat_metrics = []
+    iita_metrics = []
+    for results in simulation_results:
+        neat_simulation_metrics = []
+        iita_simulation_metrics = []
+        for neat_results, iita_results in zip(results.get('NEAT'), results.get('IITA')):
+            neat_simulation_metrics.append(neat_results.get(metric_name))
+            iita_simulation_metrics.append(iita_results.get(metric_name))
+        neat_metrics.append(neat_simulation_metrics)
+        iita_metrics.append(iita_simulation_metrics)
+    neat_matrix = np.array(neat_metrics)
+    iita_matrix = np.array(iita_metrics)
+    return (
+        neat_matrix.mean(axis=0),
+        neat_matrix.std(axis=0),
+        iita_matrix.mean(axis=0),
+        iita_matrix.std(axis=0)
+    )
 
 
-def plot_simulaion_results(results):
+def plot_simulaion_results(simulatio_results):
     fig, axes = plt.subplots(3, sharex=True)
     fig.set_size_inches(7, 8)
 
-    neat_tprs, iita_tprs = extract_metric(results, 'tpr')
-    indices = np.arange(len(neat_tprs))
+    (neat_tprs_mean,
+     neat_tprs_std,
+     iita_tprs_mean,
+     iita_tprs_std) = extract_metric(simulatio_results, 'tpr')
+
+    indices = np.arange(len(neat_tprs_mean))
 
     width = 0.35
-    axes[0].bar(indices - width / 2, neat_tprs, width, label='NEAT')
-    axes[0].bar(indices + width / 2, iita_tprs, width, label='IITA')
+    axes[0].bar(indices - width / 2, neat_tprs_mean,
+                width, label='NEAT', yerr=neat_tprs_std)
+    axes[0].bar(indices + width / 2, iita_tprs_mean,
+                width, label='IITA', yerr=iita_tprs_std)
 
     axes[0].set_title(
         '\nLearning Space Reconstruction Comparison: \nNEAT vs IITA\n', fontsize=14)
@@ -31,9 +53,15 @@ def plot_simulaion_results(results):
                              '10/60/500', '15/100/1000'))
     axes[0].legend()
 
-    neat_fprs, iita_fprs = extract_metric(results, 'fpr')
-    axes[1].bar(indices - width / 2, neat_fprs, width, label='NEAT')
-    axes[1].bar(indices + width / 2, iita_fprs, width, label='IITA')
+    (neat_fprs_mean,
+     neat_fprs_std,
+     iita_fprs_mean,
+     iita_fprs_std) = extract_metric(simulatio_results, 'fpr')
+
+    axes[1].bar(indices - width / 2, neat_fprs_mean,
+                width, label='NEAT', yerr=neat_fprs_std)
+    axes[1].bar(indices + width / 2, iita_fprs_mean,
+                width, label='IITA', yerr=iita_fprs_std)
 
     axes[1].set_ylabel('FPR')
     axes[1].set_xticks(indices)
@@ -42,12 +70,15 @@ def plot_simulaion_results(results):
     axes[1].legend()
     axes[1].set_ylim([0, 1])
 
-    neat_sizes, iita_sizes = extract_metric(results, 'size')
-    num_states, _ = extract_metric(results, 'num_states')
+    (neat_size_mean,
+     neat_size_std,
+     iita_size_mean,
+     iita_size_std) = extract_metric(simulatio_results, 'size')
+    (num_states, *_) = extract_metric(simulatio_results, 'num_states')
 
     width = 0.25
-    axes[2].bar(indices - width, neat_sizes, width, label='NEAT')
-    axes[2].bar(indices, iita_sizes, width, label='IITA')
+    axes[2].bar(indices - width, neat_size_mean, width, label='NEAT', yerr=neat_size_std)
+    axes[2].bar(indices, iita_size_mean, width, label='IITA', yerr=iita_size_std)
     axes[2].bar(indices + width, num_states, width, label='True')
 
     axes[2].set_ylabel('Number of states')
@@ -62,11 +93,13 @@ def plot_simulaion_results(results):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Plot simulation results')
-    parser.add_argument('-f', '--file', type=str,
-                        help='Path to the JSON file with results')
+    parser.add_argument('-f', '--folder', type=str,
+                        help='Path to the folder with JSON files with simulation results')
     args = parser.parse_args()
 
-    with open(args.file, 'r') as fp:
-        results = json.load(fp)
+    simulation_results = []
+    for path in glob.glob(os.path.join(args.folder, '*.json')):
+        with open(path, 'r') as fp:
+            simulation_results.append(json.load(fp))
 
-    plot_simulaion_results(results)
+    plot_simulaion_results(simulation_results)
